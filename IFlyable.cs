f95a7b737978d903fcc8266cb819b0006c81c458
Marlena Fabrick <116241357+MissMarzelous@users.noreply.github.com>
2026-06-02 15:53:53 -0400

    Create IFlyable.cs

---
+++ IFlyable.cs
@@ -0,0 +1,13 @@
+namespace OopDemo
+{
+    /// <summary>
+    /// Defines the contract for objects that are capable of flight.
+    /// </summary>
+    public interface IFlyable
+    {
+        /// <summary>
+        /// Causes the implementing object to perform a flying action.
+        /// </summary>
+        void Fly();
+    }
+}
